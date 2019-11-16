690b9719a3758971593320a242b388ca775544a3
Burak Kara <git-8f4cb@burak-kara.dev>
2019-11-16 21:31:44 +0300

    added date header to server responses

--- server/socket_test/DateTime.py
+++ server/socket_test/DateTime.py
@@ -1,5 +1,11 @@
 from datetime import datetime, timezone
 import time
+from wsgiref.handlers import format_date_time
+from time import mktime
+
+now = datetime.now()
+stamp = mktime(now.timetuple())
+print(format_date_time(stamp)) #--> Wed, 22 Oct 2008 10:52:40 GMT
 
 print(datetime.now(timezone.utc).strftime("%d %m %Y %H:%M:%S"))
 
@@ -8,7 +14,7 @@ print(millis)
 
 # current date and time
 now = datetime.now(timezone.utc)
-print(now)
+
 timestamp = datetime.timestamp(now)
 print("timestamp =", timestamp)
 print("-------------------------")
