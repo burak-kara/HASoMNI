748efba686a848b6de55d5c579964e3757754261
02485027 <kemalsarp.ozturk@kocsistem.com.tr>
2019-12-12 14:40:15 +0300

    Create config file

---
+++ src/config/config.py
@@ -0,0 +1,16 @@
+import preprocessing
+
+wifi = {'ip': '172.20.10.11',
+        'port': 8080}
+
+mobile = {'ip': '192.168.43.17',
+          'port': 8081}
+
+server = {'ip': '3.134.95.115',
+          'port': 8080}
+
+website = {'httpPort': 80,
+           'httpsPort': 443}
+
+
+use_anonymous = True
