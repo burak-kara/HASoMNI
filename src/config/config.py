b5c91c6ab84ffb9ff9765e6919cec4ebd52fa9d2
Burak Kara <git-8f4cb@burak-kara.dev>
2020-01-14 18:14:57 +0300

    added logger to Gateway.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,12 +1,12 @@
 # Primary connection info
 primary = {
-    'ip': '192.168.1.35',
+    'ip': '192.168.1.37',
     'port': 8080
 }
 
 # Secondary connection info
 secondary = {
-    'ip': '192.168.1.38',
+    'ip': '192.168.43.2',
     'port': 8081
 }
 
